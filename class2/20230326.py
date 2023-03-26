2df2fe0d7f0160ea07160b40323c2f6598ae7b88
Aaron10123 <chenyongrong0123@gmail.com>
2023-03-26 11:14:02 +0800

    class2

---
+++ class2/20230326.py
@@ -0,0 +1,17 @@
+# 在終端機輸入python可以開啟互動式執行
+# 字串'hello'、"hello"
+# 字串可以+、*
+# max() 取最大值
+# len() 取長度
+# type() 取型態
+# int() 取整數
+# float() 取浮點數
+# bool() 取布林值
+# str() 取字串
+# print(max(1,2,3,))
+# a = input("請輸入你的年齡: ")
+# print("你的年齡是" + a)
+r = int(input("圓的半徑:"))
+pi = 3.14
+Area = pi * r**2
+print("面積是:" + str(Area))
