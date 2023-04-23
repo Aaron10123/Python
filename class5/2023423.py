d3957a05a3b2b01c2eb84beb7d300e11210f8d4c
Aaron10123 <chenyongrong0123@gmail.com>
2023-04-23 11:48:39 +0800

    hw5

---
+++ class5/2023423.py
@@ -0,0 +1,31 @@
+# sum = 0
+# x = int(input('請輸入一個數字'))
+# for i in range(0, x):
+#     sum = sum + i
+# print(sum)
+
+# for a in range(1, 10):
+#     for b in range(1, 10):
+#         print(f'{a}x{b}={a*b}', end='\t')
+#     print()
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i = i + 1
+
+# password = input('輸入密碼')
+# while password != "6666":
+#     print('密碼錯誤')
+#     password = input('請重新輸入')
+# print("歡迎光臨")
+
+# i = 2
+# x = int(input('輸入數字'))
+# while x % i != 0 and i != x:
+#     i += 1
+
+# if i == x:
+#     print("yes")
+# else:
+#     print("no")
