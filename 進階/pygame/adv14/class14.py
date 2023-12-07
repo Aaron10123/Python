d50cb581134b1b8ae928611a45b2f4c38a798a15
Aaron10123 <chenyongrong0123@gmail.com>
2023-12-07 19:29:53 +0800

    class14

--- 進階/pygame/adv14/class14.py
+++ 進階/pygame/adv14/class14.py
@@ -1,17 +1,84 @@
-
 ######################載入套件######################
 import pygame
 import sys
 import os
 from pygame.locals import *
+
+
+######################物件類別#######################
+class Missile:
+    def __init__(self, x, y, image, shift):
+        """初始化飛彈"""
+        self.x = x
+        self.y = y
+        self.image = image
+        self.active = False
+        self.shift = shift
+
+    def launch(self, x, y):
+        """發射子彈"""
+        if not self.active:
+            self.x = x
+            self.y = y
+            self.active = True
+
+    def move(self):
+        """移動子彈"""
+        if self.active:
+            self.y -= self.shift
+            if self.y < 0:
+                self.active = False
+
+    def draw(self, screen):
+        """繪製飛彈"""
+        if self.active:
+            screen.blit(self.image, (self.x, self.y))
+
+
 ######################定義函式區######################
-def bg_update():
+def roll_bg():
     """更新背景"""
     global roll_y
 
     roll_y = (roll_y - 10) % bg_y
-    screen.blit(img, (0, roll_y - bg_y))  #(x , y)
-    screen.blit(img, (0, roll_y))
+    screen.blit(img, [0, roll_y - bg_y])  # (x , y)
+    screen.blit(img, [0, roll_y])
+
+
+def move_strarship():
+    """移動飛船"""
+    global ss_x, ss_y, ss_wh, ss_hh, ss_img, burn_shift
+
+    key = pygame.key.get_pressed()
+    ss_img = img_sship[0]
+    if key[pygame.K_UP]:
+        ss_y -= 20
+    if key[pygame.K_DOWN]:
+        ss_y += 20
+    if key[pygame.K_LEFT]:
+        ss_x -= 20
+        ss_img = img_sship[1]
+    if key[pygame.K_RIGHT]:
+        ss_x += 20
+        ss_img = img_sship[2]
+
+    ss_hh = ss_img.get_height() / 2
+    ss_wh = ss_img.get_width() / 2
+
+    if ss_y < ss_hh:
+        ss_y = ss_hh
+    if ss_y > bg_y - ss_hh:
+        ss_y = bg_y - ss_hh
+    if ss_x < ss_wh:
+        ss_x = ss_wh
+    if ss_x > bg_x - ss_wh:
+        ss_x = bg_x - ss_wh
+    burn_shift = (burn_shift + 2) % 6
+    screen.blit(img_burn, [ss_x - burn_w / 2, ss_y + burn_h + burn_shift])
+
+    screen.blit(ss_img, [ss_x - ss_wh, ss_y - ss_hh])
+
+
 ######################初始化設定######################
 os.chdir(sys.path[0])
 pygame.init()
@@ -23,6 +90,11 @@ BLACK = (0, 0, 0)
 clock = pygame.time.Clock()
 ######################載入圖片######################
 img = pygame.image.load("space.png")
+img_sship = [
+    pygame.image.load("fighter_M.png"),
+    pygame.image.load("fighter_L.png"),
+    pygame.image.load("fighter_R.png"),
+]
 
 
 ######################遊戲視窗設定######################
@@ -33,6 +105,13 @@ pygame.display.set_caption("Galaxy Lancer")
 screen = pygame.display.set_mode([bg_x, bg_y])
 roll_y = 0
 ######################玩家設定######################
+ss_x = bg_x / 2
+ss_y = bg_y / 2
+ss_wh = img_sship[0].get_width() / 2
+ss_hh = img_sship[0].get_height() / 2
+ss_img = img_sship[0]
+burn_shift = 0
+burn_w, burn_h = img_burn.get_rect().size
 
 ######################主程式######################
 
@@ -42,6 +121,12 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-    bg_update()
+        if event.type == KEYDOWN:
+            if event.key == K_F1:
+                screen = pygame.display.set_mode(bg_size, FULLSCREEN)
+            elif event.key == K_ESCAPE:
+                screen = pygame.display.set_mode(bg_size)
+    roll_bg()
+    move_strarship()
 
     pygame.display.update()
